49113d21d772de6ea19f1b33988df21090c04e44
abc <abc@example.com>
2022-03-14 10:03:54 +0100

    minor changes

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,15 @@
-// Mobile navigation toggle
+// Mobile navigation
 
 let openButton = document.getElementById("openBtn");
 let closeButton = document.getElementById("closeBtn");
 let mobileMenu = document.getElementById("mobileNav");
 
 openBtn.onclick = () => {
-    mobileMenu.style.display = "block";
-    openButton.style.display= "none"
-}
+  mobileMenu.style.display = "block";
+  openButton.style.display = "none";
+};
 
-closeButton.onclick = () =>{
-    mobileMenu.style.display = "none";
-    openButton.style.display = "block"
-}
+closeButton.onclick = () => {
+  mobileMenu.style.display = "none";
+  openButton.style.display = "block";
+};
